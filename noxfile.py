486b735e1b1cdf53f53a994f11f83d6adf9e298f
Brett Johnston <brett.johnston@govsco.com>
2025-04-15 15:46:46 +0000

    Proposed/iceye converter

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,10 @@ import nox
 
 os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 
-nox.options.sessions = ("lint",)
+nox.options.sessions = (
+    "lint",
+    "test",
+)
 
 
 @nox.session
@@ -17,11 +20,34 @@ def format(session):
 
 @nox.session
 def lint(session):
-    session.run_install("pdm", "sync", "--prod", "-G", "dev-lint", external=True)
+    session.run_install(
+        "pdm", "sync", "--prod", "-G", "dev-lint", "-G", "all", external=True
+    )
     session.run("ruff", "check")
     session.run(
         "ruff",
         "format",
         "--diff",
     )
-    session.run("mypy", pathlib.Path(__file__).parent / "sarkit-convert")
+    session.run("mypy", pathlib.Path(__file__).parent / "sarkit_convert")
+
+
+@nox.session
+def test(session):
+    for next_session in ("test_iceye",):
+        session.notify(next_session)
+
+
+@nox.session
+def test_iceye(session):
+    session.run_install(
+        "pdm",
+        "sync",
+        "--prod",
+        "-G",
+        "dev-test",
+        "-G",
+        "iceye",
+        external=True,
+    )
+    session.run("pytest", "tests/iceye")
---
+++ tests/iceye/test_iceye.py
@@ -0,0 +1,177 @@
+import subprocess
+
+import numpy as np
+import numpy.polynomial.polynomial as npp
+import pytest
+
+import sarkit_convert.iceye
+
+
+def test_main_smoke():
+    result = subprocess.run(
+        ["python", "-m", "sarkit_convert.iceye", "-h"],
+        capture_output=True,
+        text=True,
+    )
+    assert result.returncode == 0
+    assert "usage: iceye.py" in result.stdout
+
+
+def test_main_errors():
+    with pytest.raises(SystemExit):
+        sarkit_convert.iceye.main()
+
+    with pytest.raises(FileNotFoundError, match="Unable to synchronously open file"):
+        sarkit_convert.iceye.main(["/fake/path", "U", "/another/fake/path"])
+
+
+def test_polyfit2d():
+    x, y = np.meshgrid(np.linspace(-1, 1, 51), np.linspace(-2, 2, 53), indexing="ij")
+    rng = np.random.default_rng(seed=482025)
+    poly = rng.uniform(-1, 1, size=(3, 4))
+    z = npp.polyval2d(x, y, poly)
+    coefs = sarkit_convert.iceye.polyfit2d(x.flatten(), y.flatten(), z.flatten(), 2, 3)
+    assert np.allclose(coefs, poly)
+
+
+@pytest.mark.parametrize(
+    "x, y, z, error_msg",
+    [
+        # x not 1D
+        (np.array([[1, 2]]), np.array([1, 2]), np.array([1, 2]), "one dimensional"),
+        # y not 1D
+        (np.array([1, 2]), np.array([[1, 2]]), np.array([1, 2]), "one dimensional"),
+        # z is 3D
+        (
+            np.array([1, 2]),
+            np.array([1, 2]),
+            np.array([[[1]], [[2]]]),
+            "one or two dimensional",
+        ),
+        # mismatched lengths
+        (
+            np.array([1, 2]),
+            np.array([1, 2]),
+            np.array([[1, 2], [3, 4], [5, 6]]),
+            "same leading dimension",
+        ),
+    ],
+)
+def test_polyfit2d_invalid_inputs(x, y, z, error_msg):
+    with pytest.raises(ValueError, match=error_msg):
+        sarkit_convert.iceye.polyfit2d(x, y, z, 1, 1)
+
+
+@pytest.fixture
+def data():
+    order = 5
+    rng = np.random.default_rng(seed=482025)
+    position_poly = rng.uniform(size=(order + 1, 3))
+    time_span = (0, 1)
+    ntimes = 101
+    times = np.linspace(-10, 10, ntimes)
+    positions = npp.polyval(times, position_poly).T
+    velocities = npp.polyval(times, npp.polyder(position_poly)).T
+    accelerations = npp.polyval(times, npp.polyder(position_poly, 2)).T
+    return dict(locals())
+
+
+def test_fit_state_vectors_nom(data):
+    np.testing.assert_allclose(
+        data["position_poly"],
+        sarkit_convert.iceye.fit_state_vectors(
+            data["time_span"], data["times"], data["positions"], order=data["order"]
+        ),
+    )
+    np.testing.assert_allclose(
+        data["position_poly"],
+        sarkit_convert.iceye.fit_state_vectors(
+            data["time_span"],
+            data["times"][::10],
+            data["positions"][::10, :],
+            order=data["order"],
+        ),
+    )
+    np.testing.assert_allclose(
+        data["position_poly"],
+        sarkit_convert.iceye.fit_state_vectors(
+            data["time_span"],
+            data["times"],
+            data["positions"],
+            data["velocities"],
+            order=data["order"],
+        ),
+    )
+    np.testing.assert_allclose(
+        data["position_poly"],
+        sarkit_convert.iceye.fit_state_vectors(
+            data["time_span"],
+            data["times"],
+            data["positions"],
+            data["velocities"],
+            data["accelerations"],
+            order=data["order"],
+        ),
+    )
+    np.testing.assert_allclose(
+        data["position_poly"],
+        sarkit_convert.iceye.fit_state_vectors(
+            data["time_span"],
+            data["times"],
+            data["positions"],
+            None,
+            data["accelerations"],
+            order=data["order"],
+        ),
+    )
+
+
+def test_fit_state_vectors_insufficient_states(data):
+    dsf = 50
+    with pytest.raises(ValueError, match="Not enough state vectors"):
+        ncomp = 1
+        fail_order = (1 + (data["ntimes"] - 1) // dsf) * ncomp
+        sarkit_convert.iceye.fit_state_vectors(
+            data["time_span"],
+            data["times"][::dsf],
+            data["positions"][::dsf, :],
+            order=fail_order,
+        )
+    with pytest.raises(ValueError, match="Not enough state vectors"):
+        ncomp = 2
+        fail_order = (1 + (data["ntimes"] - 1) // dsf) * ncomp
+        sarkit_convert.iceye.fit_state_vectors(
+            data["time_span"],
+            data["times"][::dsf],
+            data["positions"][::dsf],
+            data["velocities"][::dsf],
+            order=fail_order,
+        )
+    with pytest.raises(ValueError, match="Not enough state vectors"):
+        ncomp = 3
+        fail_order = (1 + (data["ntimes"] - 1) // dsf) * ncomp
+        sarkit_convert.iceye.fit_state_vectors(
+            data["time_span"],
+            data["times"][::dsf],
+            data["positions"][::dsf],
+            data["velocities"][::dsf],
+            data["accelerations"][::dsf],
+            order=fail_order,
+        )
+    with pytest.raises(ValueError, match="Not enough state vectors"):
+        ncomp = 2
+        fail_order = (1 + (data["ntimes"] - 1) // dsf) * ncomp
+        sarkit_convert.iceye.fit_state_vectors(
+            data["time_span"],
+            data["times"][::dsf],
+            data["positions"][::dsf],
+            None,
+            data["accelerations"][::dsf],
+            order=fail_order,
+        )
+
+
+def test_broadening_from_amp_smoke():
+    amps = np.ones(1024)
+    broadening = sarkit_convert.iceye.broadening_from_amp(amps)
+    assert np.isclose(broadening, 0.8859, rtol=1e-4)
