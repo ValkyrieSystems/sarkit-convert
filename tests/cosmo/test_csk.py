3d93ef8147d9ee69adba84eb3c03eb4d5132ea12
Nathan Bombaci <Nathan.Bombaci@govsco.com>
2025-07-07 13:35:38 -0700

    Make tests work on windows

--- tests/cosmo/test_csk.py
+++ tests/cosmo/test_csk.py
@@ -1,9 +1,10 @@
 import subprocess
+import sys
 
 
 def test_usage():
     retval = subprocess.run(
-        ["python", "-m", "sarkit_convert.csk", "--help"], capture_output=True
+        [sys.executable, "-m", "sarkit_convert.csk", "--help"], capture_output=True
     )
     assert retval.returncode == 0
     assert "input_h5_file" in retval.stdout.decode()
