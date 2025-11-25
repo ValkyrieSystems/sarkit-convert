83937dbffadfe81dcee8a1eb1a990eaa7e430fe4
Nathan Bombaci <nathan.bombaci@govsco.com>
2025-11-25 17:40:18 +0000

    Utilities for creating SIDD projection polynomials from geotiff metadata

--- noxfile.py
+++ noxfile.py
@@ -13,16 +13,14 @@ nox.options.sessions = (
 
 @nox.session
 def format(session):
-    session.run_install("pdm", "sync", "--prod", "-G", "dev-lint", external=True)
+    session.run_install("pdm", "sync", external=True)
     session.run("ruff", "check", "--fix")
     session.run("ruff", "format")
 
 
 @nox.session
 def lint(session):
-    session.run_install(
-        "pdm", "sync", "--prod", "-G", "dev-lint", "-G", "all", external=True
-    )
+    session.run_install("pdm", "sync", "-G", "all", external=True)
     session.run("ruff", "check")
     session.run(
         "ruff",
@@ -36,10 +34,15 @@ def lint(session):
 def test(session):
     for next_session in (
         "test_core",
+        "test_core_dependencies",
         "test_iceye",
+        "test_iceye_dependencies",
         "test_cosmo",
+        "test_cosmo_dependencies",
         "test_tsx",
+        "test_tsx_dependencies",
         "test_sentinel",
+        "test_sentinel_dependencies",
     ):
         session.notify(next_session)
 
@@ -49,12 +52,20 @@ def test_core(session):
     session.run_install(
         "pdm",
         "sync",
-        "--prod",
-        "-G",
-        "dev-test",
         external=True,
     )
     session.run("pytest", "tests/core")
+
+
+@nox.session
+def test_core_dependencies(session):
+    session.run_install(
+        "pdm",
+        "sync",
+        "--prod",
+        external=True,
+    )
+    session.run("python", "tests/core/test_dependencies.py")
 
 
 @nox.session
@@ -62,9 +73,6 @@ def test_iceye(session):
     session.run_install(
         "pdm",
         "sync",
-        "--prod",
-        "-G",
-        "dev-test",
         "-G",
         "iceye",
         external=True,
@@ -73,13 +81,23 @@ def test_iceye(session):
 
 
 @nox.session
-def test_cosmo(session):
+def test_iceye_dependencies(session):
     session.run_install(
         "pdm",
         "sync",
         "--prod",
         "-G",
-        "dev-test",
+        "iceye",
+        external=True,
+    )
+    session.run("python", "tests/iceye/test_dependencies.py")
+
+
+@nox.session
+def test_cosmo(session):
+    session.run_install(
+        "pdm",
+        "sync",
         "-G",
         "cosmo",
         external=True,
@@ -88,13 +106,23 @@ def test_cosmo(session):
 
 
 @nox.session
-def test_tsx(session):
+def test_cosmo_dependencies(session):
     session.run_install(
         "pdm",
         "sync",
         "--prod",
         "-G",
-        "dev-test",
+        "cosmo",
+        external=True,
+    )
+    session.run("python", "tests/cosmo/test_dependencies.py")
+
+
+@nox.session
+def test_tsx(session):
+    session.run_install(
+        "pdm",
+        "sync",
         "-G",
         "tsx",
         external=True,
@@ -103,15 +131,38 @@ def test_tsx(session):
 
 
 @nox.session
-def test_sentinel(session):
+def test_tsx_dependencies(session):
     session.run_install(
         "pdm",
         "sync",
         "--prod",
         "-G",
-        "dev-test",
+        "tsx",
+        external=True,
+    )
+    session.run("python", "tests/tsx/test_dependencies.py")
+
+
+@nox.session
+def test_sentinel(session):
+    session.run_install(
+        "pdm",
+        "sync",
         "-G",
         "sentinel",
         external=True,
     )
     session.run("pytest", "tests/sentinel")
+
+
+@nox.session
+def test_sentinel_dependencies(session):
+    session.run_install(
+        "pdm",
+        "sync",
+        "--prod",
+        "-G",
+        "sentinel",
+        external=True,
+    )
+    session.run("python", "tests/sentinel/test_dependencies.py")
